832f108d9aa35c312abd96a4af7c425cb15e49ca
Nasir Lawal <nasirlawal001@gmail.com>
2021-02-14 20:42:16 +0100

    race details

---
+++ src/components/Race.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>{{ raceModel.name }}</h2>
+    <ul>
+      <li v-for="pony of raceModel.ponies" :key="pony.id">{{ pony.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { RaceModel } from '@/models/RaceModel';
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'Race',
+
+  props: {
+    raceModel: {
+      type: Object as PropType<RaceModel>,
+      required: true
+    }
+  }
+});
+</script>
