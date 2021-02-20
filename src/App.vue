76ce82a901ebe82654c148d4d4d78fc144492eee
Nasir Lawal <nasirlawal001@gmail.com>
2021-02-20 01:21:43 +0100

    suspense

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <template>
-  <Navbar />
+  <Navbar></Navbar>
   <div class="container" style="margin-top: 70px">
     <h1>Ponyracer</h1>
-    <Races></Races>
+    <Alert v-if="error" :dismissible="true">An error occurred while loading</Alert>
+    <Suspense>
+      <div>
+        <Races></Races>
+      </div>
+      <template #fallback>Loading...</template>
+    </Suspense>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Navbar from './components/Navbar.vue';
-import Races from './Views/Races.vue';
+import { defineComponent, onErrorCaptured, ref } from 'vue';
+import Navbar from '@/components/Navbar.vue';
+import Races from '@/Views/Races.vue';
+import Alert from '@/components/Alert.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     Navbar,
-    Races
+    Races,
+    Alert
+  },
+
+  setup() {
+    const error = ref<string | null>(null);
+
+    onErrorCaptured(() => {
+      error.value = 'text';
+      return false;
+    });
+
+    return { error };
   }
 });
 </script>
--- src/Views/Races.vue
+++ src/Views/Races.vue
@@ -1,13 +1,13 @@
 <template>
   <!-- Use the default variant color  -->
-  <Alert :dismissible="true"> I'm a danger alert content </Alert>
+  <!-- <Alert :dismissible="true"> I'm a danger alert content </Alert> -->
   <div v-for="race in races" :key="race.id">
     <Race :raceModel="race" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { RaceModel } from '@/models/RaceModel';
 import Race from '@/components/Race.vue';
 import { useRaceService } from '@/composables/RaceService';
@@ -18,17 +18,11 @@ export default defineComponent({
     Race
   },
 
-  setup() {
+  async setup() {
     const raceService = useRaceService();
     const races = ref<Array<RaceModel> | null>(null);
 
-    try {
-      onMounted(async () => {
-        races.value = await raceService.list();
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    races.value = await raceService.list();
 
     return { races };
   }
